53efb098ecf43e8b554f03f8f829447aea2ea598
Camilo Campo <mcp.ccampo@gmail.com>
2020-11-19 23:15:07 -0500

    added form event listeners

--- js-in-the-dom/index.js
+++ js-in-the-dom/index.js
@@ -1,12 +1,44 @@
+// variables
+let emailList = [];
+
 // elements
 const formBtn = document.querySelector('#close-form');
 const form = document.querySelector('form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const formContainer = document.querySelector('.form-container');
 
-// hide form
-function toggler(event) {
-  form.classList.toggle('hide')
-  formBtn.innerHTML === "X" ? formBtn.innerHTML = "+" : formBtn.innerHTML = "X"
-}
+// worker functions
+const toggler = function (event) {
+  form.classList.toggle('hide');
+  console.log(formBtn.innerText);
+  formBtn.innerText === 'X' ? (formBtn.innerText = '+') : (formBtn.innerText = 'X');
+};
 
-// click event
+const validateForm = function (event) {
+  if (!nameInput.value && !emailInput.value) {
+    nameInput.className = 'empty';
+    emailInput.className = 'empty';
+    alert('must enter correct info');
+  } else if (!nameInput.value) {
+    nameInput.className = 'empty';
+    alert('must enter correct info');
+  } else if (!emailInput.value) {
+    emailInput.className = 'empty';
+    alert('must enter correct info');
+  } else {
+    event.preventDefault();
+    emailList.push({ name: nameInput.value, emai: emailInput.value });
+    nameInput.value = '';
+    emailInput.value = '';
+    displayThankYou(event);
+  }
+};
+
+const displayThankYou = function (event) {
+  formContainer.innerText = 'thank you for Subscribing!...';
+};
+
+// events
 formBtn.addEventListener('click', toggler);
+form.addEventListener('submit', validateForm);
