9251f7ca2390433b3219267b133c0a4376bd3609
Camilo Campo <mcp.ccampo@gmail.com>
2020-11-19 22:14:51 -0500

    added toggler function

--- js-in-the-dom/index.js
+++ js-in-the-dom/index.js
@@ -1,1 +1,12 @@
+// elements
+const formBtn = document.querySelector('#close-form');
+const form = document.querySelector('form');
 
+// hide form
+function toggler(event) {
+  form.classList.toggle('hide')
+  formBtn.innerHTML === "X" ? formBtn.innerHTML = "+" : formBtn.innerHTML = "X"
+}
+
+// click event
+formBtn.addEventListener('click', toggler);
